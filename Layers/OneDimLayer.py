db65c821fb866ad53eb16c5cb1997773acae535d
Tan Pin Da <tan.pin.da@gmail.com>
2021-06-08 16:58:04 +0800

    added OneDimLayer base class

---
+++ Layers/OneDimLayer.py
@@ -0,0 +1,10 @@
+from Layers.LayersABC import LayerABC
+
+
+class OneDimLayer(LayerABC):
+    def __init__(self, input_length):
+        self._input_length = input_length
+
+    def forward(self, input):
+        assert (len(input.shape) == 2)
+        assert (input.shape[1] == 1)
