27a78d14a4f07a21b0537141a7a06a01f6f44231
Tan Pin Da <tan.pin.da@gmail.com>
2021-06-08 12:33:52 +0800

    edited relu

--- layers/ReLU1D.py
+++ layers/ReLU1D.py
@@ -1,6 +1,15 @@
 from layers.LayersABC import LayerABC
 
+
 class ReLU1D(LayerABC):
     def __init__(self, input_dim):
         self._input_dim = input_dim
-    def forward
+
+    def forward(self, input):
+        return input * (input > 1)
+
+
+if __name__ == '__main__':
+    print("Simple test of ReLU1D Layer");
+    test_layer = ReLU1D()
+    print(test_layer.forward(np.random.rand(16, 1)))
