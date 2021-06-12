34ff567b878ef356c36a08a7ccc589b0a79a9a27
Tan Pin Da <tan.pin.da@gmail.com>
2021-06-12 08:00:48 +0800

    generate data based on premade template on the fly

--- data_generation/generate_data.py
+++ data_generation/generate_data.py
@@ -1,23 +1,28 @@
 import numpy as np
-def generate_data(number_of_data, train_ratio,validate_ratio):
-    input_size_ = 32
-    number_of_labels_ = 2
-    threshold_ = 0
-    input_data = np.random.randn(number_of_data,input_size_,1)
-    weightage_vector = np.random.randint(low=-4,high=4,size=(input_size_,1))
-    weighted_values = input_data*weightage_vector
-    summed_values = np.sum(weighted_values,axis=1)
-    label_switch = summed_values>threshold_
-    print(label_switch)
-    truth = np.zeros((number_of_data,number_of_labels_,1))
-    # TODO find faster approach to achieve this
-    for sample_number in range(number_of_data):
-        if label_switch[sample_number,0]:
-            truth[sample_number,0,0] = 1
-        else:
-            truth[sample_number, 1, 0] = 1
+class data_generator():
+    def __init__(self,input_size):
+        self._input_size = 32
+        self._number_of_labels =2
+        self._threshold = 0
+        weightage_vector = np.random.randint(low=-4,high=4,size=(self._input_size,1))
 
-    return input_data,truth
+    def generate_data(self, batch_size):
+
+        input_data = np.random.randn(batch_size, self._input_size_, 1)
+        weightage_vector = np.random.randint(low=-4,high=4,size=(self._input_size_,1))
+        weighted_values = input_data*weightage_vector
+        summed_values = np.sum(weighted_values,axis=1)
+        label_switch = summed_values>self._threshold
+        print(label_switch)
+        truth = np.zeros((batch_size, self._number_of_labels, 1))
+        # TODO find faster approach to achieve this
+        for sample_number in range(batch_size):
+            if label_switch[sample_number,0]:
+                truth[sample_number,0,0] = 1
+            else:
+                truth[sample_number, 1, 0] = 1
+
+        return input_data,truth
 
 
 
@@ -26,4 +31,3 @@ def generate_data(number_of_data, train_ratio,validate_ratio):
 
 
 if __name__ == '__main__':
-    input,truth = generate_data(20,1,1)
